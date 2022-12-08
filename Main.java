383948a0ea69d8dce5618ea56b1313dd61f05ae7
mmmService <120030198+mmmService@users.noreply.github.com>
2022-12-08 01:28:28 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        /* Si supponga di dover definire un programma per determinare i costi di servizio legati alla
+riscossione di assegni. Il costo del servizio dipende dall’ammontare dell’assegno. Se è
+minore o uguale a 10 Euro, il costo di servizio è di 1 Euro. Se è maggiore di 10 ma minore o
+uguale a 100 Euro il costo del servizio è pari al 10% dell’importo.
+Se l’importo è maggiore di 100 Euro ma minore o uguale a 1000 Euro, il costo del servizio è
+pari a 5 Euro più il 5% dell’importo. Se il valore dell’importo è superiore a 1000 Euro, il costo
+del servizio è pari a 40 Euro più l’1% dell’importo.
+Scrivere un codice che permetta di computare questa cifra tramite un’istruzione if-else
+multi-ramo.
+
+         */
+        Scanner inputOperatore = new Scanner(System.in); // importazione classe Scanner per input tastiera
+        System.out.println("Inserisci il valore dell asegno :");//messaggio interativo
+        float valAsgn;// prilievo dati dalla tastiera e definire tipo variabile
+        valAsgn = inputOperatore.nextFloat();
+
+        float costoServizio; //definizione variabile
+
+        if (valAsgn<=10) {
+            costoServizio = 1;
+            System.out.println("Per il tuo asegno da " +valAsgn + " euro, devi pagare " + costoServizio + " euro");
+        }else if (valAsgn>10 && valAsgn<=100) {
+            costoServizio = valAsgn * 0.1f; // 10%
+            System.out.println("Per il tuo asegno da " + valAsgn + " euro, devi pagare " + costoServizio + " euro");
+        }else if (valAsgn>100 && valAsgn<=1000){
+            costoServizio = valAsgn * 0.05f + 5;// 5% + 5
+            System.out.println("Per il tuo asegno da " + valAsgn + " euro, devi pagare " + costoServizio + " euro");
+        }else if (valAsgn > 1000){
+            costoServizio = valAsgn * 0.01f + 40;// 1% + 40
+            System.out.println("Per il tuo asegno da " + valAsgn + " euro, devi pagare " + costoServizio + " euro");
+        }
+    }
+}
